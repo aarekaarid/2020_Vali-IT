5c17147a81ef3c9f0c4940f62a9acdb88b6e4692
aarekaarid <aare.kaarid@gmail.com>
2020-11-04 13:35:51 +0200

    Initial commit

---
+++ main/java/ee/bcs/valiit/tasks/Lesson1.java
@@ -0,0 +1,72 @@
+package ee.bcs.valiit.tasks;
+
+public class Lesson1 {
+
+    public static void main(String[] args) {
+        excersie1();
+        excersie2();
+        excersie3();
+        excersie4();
+    }
+
+    public static void excersie1() {
+        // TODO
+        // defineeri 3 muutujat a = 1, b = 1, c = 3
+        int a = 1;
+        int b = 1;
+        int c = 3;
+
+        // Prindi välja a==b ja a==c
+        System.out.println(a == c);
+        System.out.println(a == b);
+        // Lisa rida a = c
+        a = c;
+        // Prindi välja a==b ja a==c, mis muutus???
+        System.out.println(a == b);
+        System.out.println(a == c);
+    }
+
+    public static void excersie2() {
+        // TODO
+        // Loo muutujad x1 = 10 ja x2 = 20, vali sobiv andmetüüp
+        int x1 = 10;
+        int x2 = 20;
+        // Tekita muutuja y1 = ++x1, trüki välja nii x1 kui y1
+        int y1 = ++x1;  //x1 = x1 + 1 on viimane tehe
+        System.out.println("x1=" + x1);
+        System.out.println("y1=" + y1);
+
+        // Tekita muutuja y2 = x2++, trüki välja nii x2 ja y2
+        int y2 = x2++;
+        System.out.println("x2=" + x2);
+        System.out.println("y2=" + y2);
+        // Analüüsi tulemusi
+    }
+
+    public static void excersie3() {
+        // TODO
+        // Loo arvulised muutujad
+        // a = 18 % 3
+        // b = 19 % 3
+        // c = 20 % 3
+        // d = 21 % 3
+        int a = 18 % 3;
+        int b = 19 % 3;
+        int c = 20 % 3;
+        int d = 21 % 3;
+        // Prindi välja kõigi muutujate väärtused
+        System.out.println("a=" + a);
+        System.out.println("b=" + b);
+        System.out.println("c=" + c);
+        System.out.println("d=" + d);
+    }
+
+    public static void excersie4() {
+        // TODO
+        // Defineeri String tüüpi muutuja mille sisu oleks "\"\\""
+        String string = "\"\\\"\\\\\"\"";
+        // Trüki muutuja sisu välja
+        System.out.println(string);
+    }
+
+}
---
+++ main/java/ee/bcs/valiit/tasks/Lesson3.java
@@ -0,0 +1,118 @@
+package ee.bcs.valiit.tasks;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class Lesson3 {
+
+    public static void main(String[] args) {
+//        System.out.println(uusSumma(3, 3));
+//        System.out.println(sum(new int[] {2,6,98,64674}));  // declaring array!!!
+//        System.out.println(factorial(5));
+//        System.out.println(sort(new int[]{1, 4, -1, 256}));
+        System.out.println(reverseString("luuk"));
+
+    }
+
+
+//    public static void main(String[] args) {
+//        List<Integer> testList = new ArrayList<>();
+//        testList.add(4);
+//        testList.add(5);
+//        testList.add(6);
+//        Arrays.toString(new int[0]);
+//        for (Integer i : testList) {
+//            System.out.println(i);
+//        }
+//
+//        System.out.print(testList);
+//
+//        System.out.println(reverseString("Test1 Test2"));
+//        Map<String, String> testMap = new HashMap<>();
+//        test(testMap);
+//        Object a;
+//    }
+
+    private static void test(Map<String, String> testMap) {
+        testMap.put("test", "test");
+    }
+
+    public static int uusSumma(int x, int y) {
+        // TODO tagasta x ja y summa vaartus
+        return x+y;
+    }
+
+    public static int sum(int[] x){
+        // Todo liida kokku kõik numbrid massivis x
+
+        int asum = x[0];
+        for (int i = 1; i < x.length; i++) {
+            asum = asum + x[i];
+        }
+        return asum;
+    }
+
+    public static int factorial(int x) {
+        // TODO tagasta x faktoriaal.
+        // Näiteks
+        // x = 5
+        // return 4*3*2*1 = 24
+        int fac = 1;
+        for (int i = 2; i < x; i++) {
+            fac = fac * i;
+        }
+        return fac;
+    }
+
+    public static int[] sort(int[] a) {
+        // TODO sorteeri massiiv suuruse järgi
+        // Näiteks {2, 6, 8, 1}
+        // Väljund {1, 2, 6, 8}
+        System.out.println(Arrays.toString(a));
+        // Outer loop changes the number of comparable cells
+        for (int i = 0; i < a.length; i++){
+              int min = a[i];
+              int index = i;
+              // Inner loop goes through the current array (array is 1 shorter every outer loop)
+            for (int j = i; j < a.length; j++){
+                if(a[j] < min) {
+                    min = a[j];
+                    index = j;
+//                    System.out.println(min);
+//                    System.out.println(j);
+                }
+            }
+            // Replacing the min and current first value with each other after every outer loop,
+            int tmp = a[i];
+            a[i] = min;
+            a[index] = tmp;
+        }
+        System.out.println(Arrays.toString(a));
+        return a;
+    }
+
+    public static String reverseString(String a) {
+        // TODO tagasta string tagurpidi
+        // Näiteks:
+        // a = "Test";
+        // return tseT";
+        System.out.println(a);
+        // Use StringBuilder when manipulating Strings
+        StringBuilder word = new StringBuilder();
+        for (int i = a.length(); i >= 0; i--) {
+            word.append(i);
+        }
+
+        System.out.println(word);
+
+//        System.out.println(a.substring(1));
+
+        return "";
+    }
+
+    public static boolean isPrime(int x){
+        // TODO tagasta kas sisestatud arv on primaar arv (jagub ainult 1 ja iseendaga)
+        return false;
+    }
+
+}
